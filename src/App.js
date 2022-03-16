79481ab5128efb2f80265eb751d08e7becccdd00
Alccro <farnelali@gmail.com>
2022-03-16 16:35:07 +0100

    finished basic functionality

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
 import React, { Component } from "react";
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { friends } from './friends';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            friends: friends,
+            friends: [],
             searchField: ''
         }
     }
+
+componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({friends: users}));
+}
 
     onSearchChange= (event) => {
         this.setState({ searchField: event.target.value })
     }
     
     render() {
-        const filteredFriends = this.state.friends.filter(friends => {
-            return friends.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        const filteredFriends = this.state.friends.filter(friend => {
+            return friend.name.toLowerCase().includes(this.state.searchField.toLowerCase());
         })
-        return (
-           <div  className='tc'>
-               <h1 className='fw7 b f1 hot-pink'>Robofriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList friends={filteredFriends} />
-            </div>
-        );
+        if (this.state.friends.length === 0) {
+            return <h1 className='tc'>Give me a sec to find some friends for you</h1>
+        } else {
+            return (
+               <div  className='tc'>
+                   <h1 className='fw7 b f1 hot-pink'>Loyal Friends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <CardList friends={filteredFriends} />
+                    </Scroll>
+                </div>
+            );
+        }
     }
 }
 
